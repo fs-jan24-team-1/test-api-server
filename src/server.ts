a9e403df28e9a7dac9ac4167ad657c23c96c8b90
Oleh Duma <hakerprof@gmail.com>
2024-05-28 16:30:12 +0300

    changed origin in cors

--- src/server.ts
+++ src/server.ts
@@ -14,7 +14,7 @@ app.use(express.json());
 
 app.use(
   cors({
-    origin: 'https://fs-jan24-team-1.github.io/phone-catalog-fs24jan/',
+    origin: 'https://fs-jan24-team-1.github.io',
     credentials: true,
   }),
 );
